03bfe2c107b0f70e601cb512db7a47dc825972d7
netspaceengineer <jharrisab.ece@gmail.com>
2021-12-03 17:09:20 +0800

    Adding Quick Object(Web Entity) Test

--- src/main/java/app/seleniumap/misc/StepThreadRunner.java
+++ src/main/java/app/seleniumap/misc/StepThreadRunner.java
@@ -8,13 +8,15 @@ import org.openqa.selenium.WebDriver;
 public class StepThreadRunner implements Runnable {
     private WebDriver driver;
     private TestStep testStep;
-    public  StepThreadRunner(TestStep testStep, WebDriver driver){
+    private String variant;
+    public  StepThreadRunner(TestStep testStep, WebDriver driver, String variant){
         this.driver = driver;
         this.testStep = testStep;
+        this.variant = variant;
     }
     @Override
     public void run() {
-        Result res = new Executor(driver,testStep).execute();
+        Result res = new Executor(driver,testStep, variant).execute();
         System.out.println(res.getResult());
     }
 }
